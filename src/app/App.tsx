ebd27278e542a641189ba559fcc83432801d79af
murakami <murakami77@nifty.com>
2025-11-05 23:00:32 +0900

    ヒグマ出没データの著作権表示を追記

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -19,6 +19,15 @@ const App = (): JSX.Element => {
             &copy; OpenStreetMap
           </a>
         </p>
+        <p>
+          ヒグマ出没データ {' '}
+          <a href="https://ckan.pf-sapporo.jp/dataset/sapporo_bear_appearance" target='_blank' rel="noopener noreferrer">
+            &copy; 	札幌市環境局環境都市推進部環境管理担当課
+          </a>
+          (<a href="https://creativecommons.org/licenses/by/4.0/deed.ja">
+            クリエイティブ・コモンズ・ライセンス（表示4.0 国際）
+          </a>)を改変して作成
+        </p>
       </footer>
     </div>
   );
